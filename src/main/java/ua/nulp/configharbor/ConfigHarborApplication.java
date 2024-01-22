0326e51f25e9a11b9dcc93317ddb8db8e448bb1e
Andrii Tyzun <mrandrewtest@gmail.com>
2024-01-22 10:29:25 +0200

    Initial commit

---
+++ src/main/java/ua/nulp/configharbor/ConfigHarborApplication.java
@@ -0,0 +1,13 @@
+package ua.nulp.configharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigHarborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigHarborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/nulp/configharbor/ConfigHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nulp.configharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigHarborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
